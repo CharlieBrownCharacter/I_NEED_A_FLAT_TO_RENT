38b8f4212e55e41c13afe0c986d2cfd15890c053
CharlieBrownCharacter <bruno11.francisco@gmail.com>
2019-09-04 14:32:57 +0100

    - finished script (I WILL FIND THAT PERFECT FLAT)

--- constants.py
+++ constants.py
@@ -1,2 +1,5 @@
 PAGE = 'https://www.njuskalo.hr/iznajmljivanje-soba'
 EMAIL = 'bruno11.francisco@gmail.com'
+FILE_LOCATION = '/tmp/'
+FILE_NAME = 'latest_item_njuskalo.txt'
+SECONDS_TO_CALL_FUNCTION = 60
